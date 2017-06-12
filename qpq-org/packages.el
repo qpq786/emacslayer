5cd69f212505bf9321255ffb48f67b5794dd9939
qinpq <qinpqcu@gmail.com>
2017-06-12 18:07:43 +0900

    add shift todo item in the org file

--- qpq-org/packages.el
+++ qpq-org/packages.el
@@ -104,4 +104,12 @@
    '(org-headline-done
      ((((class color) (min-colors 16) (background dark))
        (:foreground "#b58900" :strike-through t)))))
+
+  ;;Switch entry to DONE when all subentries are done, to TODO otherwise.
+  (defun org-summary-todo (n-done n-not-done)
+    "Switch entry to DONE when all subentries are done, to TODO otherwise."
+    (let (org-log-done org-log-states)   ; turn off logging
+      (org-todo (if (= n-not-done 0) "DONE" "TODO"))))
+  (add-hook 'org-after-todo-statistics-hook 'org-summary-todo)
+
   )
