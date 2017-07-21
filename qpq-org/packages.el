686b232b303e0560a6fdc3d2a4f09e0e9d89021a
qinpq <qinpqcu@gmail.com>
2017-07-21 17:49:34 +0900

    modify org file

--- qpq-org/packages.el
+++ qpq-org/packages.el
@@ -64,11 +64,14 @@
         '(("t" "Todo-today" entry (file+headline org-agenda-file-gtd "Today")
            "* TODO thinking, solving and finding the key problem in%?\nkey task: \nkey problem:\n  %i\n"
            :empty-lines 1)
-          ("d" "Day Report")
-          ("dr" "Day-report" entry (file+headline org-agenda-file-dayreport "Day Report")
-           "* %?\n** 计划\n1. \n** 时间\n|上午|||\n|下午|||\n|有效时间||\n** 完成与否？\n\n** 轻松／吃力？\n\n** 调整\n\n** 奖惩\n\n** 改进\n"
+          ("r" "Report")
+          ("rd" "Day-report" entry (file+headline org-agenda-file-dayreport "Day Report")
+           "* %?\n** 计划\n1. \n** 时间\n|上午|||\n|下午|||\n|有效时间||\n** 完成与否？\n\n** 轻松／吃力？\n\n** 调整／改进\n\n** 奖惩"
            :empty-lines 1)
-          ("dl" "Day-life-report" entry (file+headline org-agenda-file-daylifereport "Life Report")
+          ("rw" "week-report" entry (file+headline org-agenda-file-dayreport "Day Report")
+           "* %?月 周\n** 时间\n|有效时间||\n|平均有效时间||\n** 周目标完成与否？\n\n** 轻松／吃力？\n\n** 调整／改进\n\n** 奖惩"
+           :empty-lines 1)
+          ("lr" "Day-life-report" entry (file+headline org-agenda-file-daylifereport "Life Report")
            "* %?\n** 食物\n上午： \n中午： \n下午： \n** 饮水\n\n** 工作状态与心情\n\n** 改进\n"
            :empty-lines 1)
           ("e" "emacs-learing" entry (file+headline org-agenda-file-emacs "Emacs")
